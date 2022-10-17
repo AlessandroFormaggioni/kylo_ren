7e2b204a83b23f815f6b89d6f0aa06219d947366
AlessandroFormaggioni <85106688+AlessandroFormaggioni@users.noreply.github.com>
2022-10-18 00:54:02 +0200

    Update overlap_tab.py

--- overlap_tab.py
+++ overlap_tab.py
@@ -1,26 +1,30 @@
 import re
-import pandas ad pd
+import pandas as pd
 
 with open("overlap_30_output","r") as r:
 	file=[a for a in r.readlines() if a[0] == ">"]
 output = pd.DataFrame()
 line=0
-while line <= len(file):
+while line < len(file):
 	dict={}
 	sign=file[line].split("|")[2].split(" ")[1]
 	dict["cord_"+sign]=file[line].split("|")[1].split("=")[1]
 	n_reads=int(re.split('=|\n',file[line].split("|")[4])[1])
-	while (file[line].split("|")[1] == file[line+1].split("|")[1]) AND (file[line].split("|")[2] == file[line+1].split("|")[2]):
+	while (file[line].split("|")[1] == file[line+1].split("|")[1]) and (file[line].split("|")[2] == file[line+1].split("|")[2]):
 		line+=1
 		n_reads+=int(re.split('=|\n',file[line].split("|")[4])[1])
 	dict["n_reads_"+sign]=n_reads
 	line+=1
 	sign=file[line].split("|")[2].split(" ")[1]
 	dict["cord_"+sign]=file[line].split("|")[1].split("=")[1]
-	while (file[line].split("|")[1] == file[line+1].split("|")[1]) AND (file[line].split("|")[2] == file[line+1].split("|")[2]):
-		line+=1
-		n_reads+=int(re.split('=|\n',file[line].split("|")[4])[1])
+	if line != len(file)-1:
+		while (file[line].split("|")[1] == file[line+1].split("|")[1]) and (file[line].split("|")[2] == file[line+1].split("|")[2]):
+			line+=1
+			n_reads+=int(re.split('=|\n',file[line].split("|")[4])[1])
+			if line == len(file)-1:
+				break
 	dict["n_reads_"+sign]=n_reads
-	output = output.append(dict, ignore_index=True)
-  
-  print(output)
+
+output = output.append(dict, ignore_index=True)
+
+	
